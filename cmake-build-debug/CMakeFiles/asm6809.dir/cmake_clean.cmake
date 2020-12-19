18d75eff09a59aaec5070eecfd8aa9abd091f2dd
Kiyle Winborne <kiyleawinborne@gmail.com>
2020-12-18 20:47:47 -0500

    Added cmake files and current binaries
    Just updating master to include files forgotten in initial commit.

---
+++ cmake-build-debug/CMakeFiles/asm6809.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asm6809.dir/main.f90.o"
+  "asm6809.exe"
+  "asm6809.pdb"
+  "libasm6809.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/asm6809.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
